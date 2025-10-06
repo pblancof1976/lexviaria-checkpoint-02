55a43d6ac6f34865a6cc7ad1e9912fa3855c7fb7
pblancof1976 <pblancof1976@gmail.com>
2025-10-06 12:17:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 Muy importante para GitHub Pages (nombre EXACTO del repo)
 export default defineConfig({
+  base: '/lexviaria-checkpoint-02/',
   plugins: [react()],
 })
